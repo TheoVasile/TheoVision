66f64b893e01b2e6ecebddc8df8b39d9773765fa
Theo Vasile <theovasile@theos-laptop.home>
2024-03-31 00:40:15 -0400

    updated collision script for rays and triangles

--- shaders/ray.cpp
+++ shaders/ray.cpp
@@ -38,32 +38,40 @@ Mesh *Ray::getCollisionMesh() {
 void Ray::cast(array<vec3, 3> tri)
 {
     this->hasHit = false;
-    // Find the point of intersection with the plane
-    vec3 normal = getNormal(tri);
-    float numerator = -dot(normal, (this->pos - tri[0]));
-    float denominator = dot(normal, this->direction);
-    if (denominator == 0) {
+    constexpr float epsilon = std::numeric_limits<float>::epsilon();
+    vec3 edge1 = tri[1] - tri[0];
+    vec3 edge2 = tri[2] - tri[0];
+    vec3 ray_cross_edge2 = cross(this->direction, edge2);
+    float determinant = dot(edge1, ray_cross_edge2);
+
+    // Check if ray is parallel to the triangle
+    if (determinant > -epsilon && determinant < epsilon) {
         return;
     }
-    float t = numerator / denominator;
-    if (t <= 0) {
+
+    float inverse_determinant = 1.0 / determinant;
+    vec3 s = this->pos - tri[0];
+    float u = inverse_determinant * dot(s, ray_cross_edge2);
+
+    if (u < 0 || u > 1) {
         return;
     }
-    vec3 collision = this->pos + (this->direction * t);
 
-    // See if the point lies within the tri.
-    vec3 v1 = collision - tri[0];
-    vec3 v2 = collision - tri[1];
-    vec3 v3 = collision - tri[2];
-    float angle = acos(dot(v1, v2) / (length(v1) * length(v2)));
-    angle += acos(dot(v3, v2) / (length(v3) * length(v2)));
-    angle += acos(dot(v3, v1) / (length(v3) * length(v1)));
+    vec3 s_cross_edge1 = cross(s, edge1);
+    float v = inverse_determinant * dot(this->direction, s_cross_edge1);
 
-    if (abs(angle - 2 * M_PI) < 0.1) {
-        this->collisionPoint = collision;
+    if (v < 0 || u+v > 1) {
+        return;
+    }
+
+    // Compute point where ray intersects triangle
+    float t = inverse_determinant * dot(edge2, s_cross_edge1);
+
+    if (t > epsilon) {
+        this->collisionPoint = this->pos + this->direction * t;
         this->collisionNormal = getNormal(tri);
         this->outgoingDirection = this->direction - 2 * dot(this->collisionNormal, this->direction) * this->collisionNormal;
-    this->hasHit = true;
+        this->hasHit = true;
     }
 }
 
